09f13a8c4d01d096ee271c2f481085b29c626551
eunjoo0311 <120389195+eunjoo0311@users.noreply.github.com>
2026-01-10 22:54:03 +0900

    Time: 1 ms (86.77%), Space: 54.8 MB (74.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,13 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-
 var twoSum = function(nums, target) {
+    const map = new Map();
     for(let i = 0; i<nums.length; i++) {
-        for(let j = i+1; j< nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
+      const need = target - nums[i]
+
+      if(map.has(need)) {
+        return[map.get(need), i]
+      }
+      map.set(nums[i],i)
     }
 };
