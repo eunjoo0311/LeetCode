c3879b575f542792875dced4a24edca9e9359606
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-14 20:38:38 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.01 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,9 @@
 function solution(numbers) {
     const result = []
-    for(let i= 0; i<numbers.length-1; i++ ){
+    for(let i = 0; i<numbers.length -1; i++) {
         for(let j = i + 1; j<numbers.length; j++) {
             result.push(numbers[i] + numbers[j])
         }
     }
-    const setResult = new Set(result)
-    return [...setResult].sort((a,b) => a-b)
+    return result.sort((a,b) => a-b).filter((v,i) => v !== result[i-1])
 }
