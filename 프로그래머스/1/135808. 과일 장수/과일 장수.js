c13cf903938326051695304814a97d6d1d10029b
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-15 00:42:04 +0900

    [level 1] Title: 과일 장수, Time: 211.34 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,15 +1,9 @@
 function solution(k, m, score) {
     score.sort((a,b) => b-a)
-    score.splice(score.length - score.length % m)
+    
     let price = 0;
-    const row = []
-    for(let i = 0; i<score.length; i++) {
-        if(i % m === 0) {
-          row.push(score.slice(i,i+m))
-        }
-    }
-    for(let i = 0; i<row.length; i++) {
-        price += Math.min(k, ...row[i]) * m
+    for(let i = m-1; i<score.length; i+=m) {
+        price += score[i] * m
     }
     return price
 }
