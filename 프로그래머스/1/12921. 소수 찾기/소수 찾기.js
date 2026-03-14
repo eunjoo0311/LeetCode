24bd3ce4ac62bdf49ddeef17fbe6f94914ce6ab5
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-15 01:38:29 +0900

    [level 1] Title: 소수 찾기, Time: 53.36 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const arr = new Array(n+1).fill(true)
+    
+    arr[0] = false
+    arr[1] = false
+    
+    for(let i = 2; i*i<= n; i++) {
+        if(arr[i]) {
+            for(let j = i*i; j<=n; j+=i) {
+                arr[j] = false
+            }
+        }
+    }
+    return arr.filter(v => v).length
+}
