ecc16e1e6161939d7870e48110879db04cde034d
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-15 18:59:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.38 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,22 +1,4 @@
 function solution(participant, completion) {
-    const map = {}
-    
-    for(let people of participant) {
-        if(map[people] === undefined) {
-            map[people] = 1
-        } else {
-            map[people] ++
-        }
-    }
-    
-    for(let complete of completion) {
-        if(map[complete]) {
-            map[complete] --
-        }
-    }
-    for(let key in map) {
-        if(map[key] !== 0) {
-            return key
-        }
-    }
+    var answer = '';
+    return answer;
 }
