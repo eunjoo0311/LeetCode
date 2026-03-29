731fa7e576be6832dba601fff60492aad418980a
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-29 22:01:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,22 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return [...s].sort().join('') === [...t].sort().join('')
+    if(s.length !== t.length) return false;
+
+    const map = {}
+
+    for(let char of s) {
+        if(map[char]) {
+            map[char]++
+        } else {
+            map[char] = 1
+        }
+    }
+
+    for(let char of t) {
+        if(!map[char]) return false
+        map[char]--
+    }
+
+    return true
 };
