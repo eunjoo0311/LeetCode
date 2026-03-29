c7e1f4d36e232be001b6a3f1f5331c8399d4747c
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-03-29 21:55:45 +0900

    Time: 28 ms (19.09%), Space: 57.6 MB (49.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,7 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const s1 = [...s].sort().join('')
-    const s2 = [...t].sort().join('')
-    return s1 === s2 ? true : false
+    return [...s].sort().join('') === [...t].sort().join('')
 };
