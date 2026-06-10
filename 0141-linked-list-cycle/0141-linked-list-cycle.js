8a7d5767ef4a4d7b6c8d5fe97fa372319b8a9016
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-06-10 23:02:57 +0900

    Time: 41 ms (97.55%), Space: 58.5 MB (78.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -13,12 +13,11 @@
 var hasCycle = function(head) {
     let slow = head
     let fast = head
-
     while(fast !== null && fast.next !== null) {
         slow = slow.next
         fast = fast.next.next
 
-        if(fast === slow) {
+        if(slow === fast) {
             return true
         }
     }
