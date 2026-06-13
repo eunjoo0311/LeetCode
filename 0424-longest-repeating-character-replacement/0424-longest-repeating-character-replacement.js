9e1d3370b2baacebc98714a3bd8baf50f487f621
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-06-13 10:08:34 +0900

    Time: 19 ms (55.55%), Space: 53.6 MB (99.81%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -3,24 +3,22 @@
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function (s, k) {
-    const count = {}
+var characterReplacement = function(s, k) {
+    const map = {}
 
-    let left = 0;
+    let left = 0
     let maxCount = 0;
-    let answer = 0;
+    let answer = 0
 
-    for (let right = 0; right < s.length; right++) {
-        const char = s[right]
-        count[char] = (count[char] || 0) + 1
+    for(let right = 0; right < s.length; right++) {
+        map[s[right]] = (map[s[right]] || 0) + 1
+        maxCount = Math.max(maxCount, map[s[right]])
 
-        maxCount = Math.max(maxCount, count[char])
-
-        while ((right - left + 1) - maxCount > k) {
-            count[s[left]]--
+        while(right - left + 1 - maxCount > k) {
+            map[s[left]]--
             left++
         }
-        answer = Math.max(answer, right - left + 1)
+        answer = Math.max(answer, right- left + 1)
     }
-    return answer
+    return  answer
 };
