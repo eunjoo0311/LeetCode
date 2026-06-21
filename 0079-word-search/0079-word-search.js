a66725c0cb81a6016055f4baa571ecf19ba3ffaa
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-06-21 19:15:30 +0900

    Time: 301 ms (72.32%), Space: 54.1 MB (73.57%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,43 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const rows = board.length
+    const cols = board[0].length
+
+    const dfs = (row, col, index) => {
+        if(index === word.length) {
+            return true
+        }
+
+        if(row < 0 || row >= rows || col < 0 || col >= cols) {
+            return false
+        }
+
+        if(board[row][col] !== word[index]) return false
+
+        const temp = board[row][col]
+        board[row][col] = '#'
+        
+        const found = dfs(row + 1, col, index + 1) ||
+        dfs(row - 1, col, index + 1) ||
+        dfs(row, col - 1, index + 1) ||
+        dfs(row, col + 1, index + 1) 
+
+        board[row][col] = temp
+        
+        return found
+    }
+
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if(dfs(row, col, 0)) {
+                return true
+            }
+        }
+    }
+
+    return false
+};
