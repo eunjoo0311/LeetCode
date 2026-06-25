419543646c328b328db44c09db7619109095d2e5
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-06-26 01:07:41 +0900

    Time: 8 ms (91.26%), Space: 62.1 MB (64.73%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+    const graph = Array.from({ length: numCourses }, () => [])
+
+    for (let [a, b] of prerequisites) {
+        graph[b].push(a)
+    }
+
+    const visited = Array(numCourses).fill(0)
+
+    const dfs = (course) => {
+        if (visited[course] === 1) {
+            return false
+        }
+        if (visited[course] === 2) {
+            return true
+        }
+
+        visited[course] = 1;
+
+        for(let next of graph[course]) {
+            if(!dfs(next)) {
+                return false
+            }
+        }
+        visited[course] = 2
+        return true
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) {
+            return false
+        }
+    }
+
+    return true
+};
