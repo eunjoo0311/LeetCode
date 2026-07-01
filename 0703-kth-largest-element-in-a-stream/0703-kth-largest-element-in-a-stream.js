dee0516afd8255737c8b1a967f66d7698920576b
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-01 18:11:50 +0900

    Time: 32 ms (56.35%), Space: 68.6 MB (51.52%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,93 @@
+class KthHeap {
+    constructor(compare) {
+        this.heap = []
+        this.compare = compare
+    }
+    size() {
+        return this.heap.length
+    }
+    peek() {
+        return this.heap[0]
+    }
+    push(value) {
+        this.heap.push(value)
+        this.bubbleUp()
+    }
+    pop() {
+        if (this.size() === 0) return null
+        if (this.size() === 1) return this.heap.pop()
+        const top = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.bubbleDown()
+        return top
+    }
+    bubbleUp() {
+        let index = this.size() - 1
+
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2)
+
+            if (this.compare(this.heap[parentIndex], this.heap[index])) {
+                break
+            }
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
+            index = parentIndex
+        }
+    }
+    bubbleDown() {
+        let index = 0
+        while (true) {
+            let targetIndex = index
+
+            let leftIndex = index * 2 + 1
+            let rightIndex = index * 2 + 2
+
+            if (leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
+                targetIndex = leftIndex
+            }
+
+            if (rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
+                targetIndex = rightIndex
+            }
+
+            if (index === targetIndex) {
+                break
+            }
+            [this.heap[index], this.heap[targetIndex]] = [this.heap[targetIndex], this.heap[index]]
+            index = targetIndex
+        }
+    }
+}
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+    this.k = k
+    this.minHeap = new KthHeap((a, b) => a < b)
+
+    for (let num of nums) {
+        this.add(num)
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+    this.minHeap.push(val)
+
+    if (this.minHeap.size() > this.k) {
+        this.minHeap.pop()
+    }
+
+    return this.minHeap.peek()
+};
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
