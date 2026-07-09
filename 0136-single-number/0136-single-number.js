9d524a2775c4a4e5788a3463ecb2bc59a991910f
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-09 23:34:13 +0900

    Time: 11 ms (29.43%), Space: 59.6 MB (17.38%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    const map = {}
+
+    for (let num of nums) {
+        map[num] = (map[num] || 0) + 1
+    }
+
+    for (let num of nums) {
+        if (map[num] === 1) {
+            return num
+        }
+    }
+};
