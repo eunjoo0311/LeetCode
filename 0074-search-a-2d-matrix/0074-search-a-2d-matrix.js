24186a43c9dced88dec40530a2b8c14eb7ffdb6f
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-10 22:36:03 +0900

    Time: 0 ms (100%), Space: 54.2 MB (27.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,25 +3,29 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function (matrix, target) {
-    const m = matrix.length;
-    if (m === 0) return false;
-    const n = matrix[0].length;
-    if (n === 0) return false;
+var searchMatrix = function(matrix, target) {
+    const rows = matrix.length
+    const cols = matrix[0].length
 
     let left = 0;
-    let right = m * n - 1
+    let right = rows * cols - 1
 
-    while (left <= right) {
+    while(left <= right) {
         const mid = Math.floor((left + right) / 2)
-        const r = Math.floor(mid / n)
-        const c = mid % n;
-        const value = matrix[r][c]
 
-        if (value === target) return true;
-        if (value < target) left = mid + 1
-        else right = mid - 1
+        const row = Math.floor(mid / cols)
+        const col = mid % cols
+
+        const value = matrix[row][col]
+
+        if(value === target) {
+            return true
+        } else if (value < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
     }
 
-    return false;
+    return false
 };
