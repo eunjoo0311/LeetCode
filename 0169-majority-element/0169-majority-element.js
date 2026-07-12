ed2ad70223a27ce154d9563c9efad5fc6183efe1
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-12 21:49:50 +0900

    Time: 6 ms (36.97%), Space: 58.6 MB (23.27%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = {}
+
+    for(let num of nums) {
+        map[num] = (map[num] || 0) + 1
+
+        if(map[num] > nums.length / 2)  {
+            return num
+        }
+    }
+};
