3fa7185e3316a1db6abf8daaa42d01c91be92a7f
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-13 23:39:23 +0900

    Time: 0 ms (100%), Space: 52.9 MB (92.89%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    const map = {}
+
+    for(let num of nums) {
+        map[num] = (map[num] || 0) + 1
+    }
+
+    let index = 0;
+
+    for(let i = 0; i <= 2; i++) {
+        for(let j = 0; j < map[i]; j++) {
+            nums[index] = i
+            index++
+        }
+    }
+};
