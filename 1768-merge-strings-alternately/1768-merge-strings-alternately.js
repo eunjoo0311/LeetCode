15df778cdd3e06d91db6e135ad433cade82205e1
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-16 23:21:52 +0900

    Time: 52 ms (18.99%), Space: 54.1 MB (48.58%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let answer = "";
+    const maxLength = Math.max(word1.length, word2.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        if (i < word1.length) {
+            answer += word1[i];
+        }
+
+        if (i < word2.length) {
+            answer += word2[i];
+        }
+    }
+
+    return answer;
+};
