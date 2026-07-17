6e87c728b02b79061f28a6d6a8a38ccf44d4fc3a
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-18 03:54:56 +0900

    Time: 2 ms (64.46%), Space: 56.6 MB (38.07%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let left = 0;
+    let left = 0
     let right = numbers.length - 1
 
-    while(left < right) {        
+    while(left < right) {
         const sum = numbers[left] + numbers[right]
 
         if(sum === target) {
