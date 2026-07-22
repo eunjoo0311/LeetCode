a93f88c6a319485ecedfd3fc84c6dc4cca00df64
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-22 21:47:26 +0900

    Time: 190 ms (48.48%), Space: 92.5 MB (23.65%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -4,18 +4,27 @@
  * @param {number[]} speed
  * @return {number}
  */
-var carFleet = function(target, position, speed) {
-    const cars = position.map((p, i) => [p, speed[i]])
-    cars.sort((a,b) => b[0] - a[0])
+var carFleet = function (target, position, speed) {
+    const cars = [];
+
+    for (let i = 0; i < position.length; i++) {
+        cars.push([position[i], speed[i]]);
+    }
+
+    cars.sort((a, b) => b[0] - a[0]);
 
     const stack = [];
 
-    for(const [p,s] of cars) {
-        const time = (target - p) /s;
+    for (const [currentPosition, currentSpeed] of cars) {
+        const time = (target - currentPosition) / currentSpeed;
 
-        if(stack.length === 0 || time>stack[stack.length -1]) {
-            stack.push(time)
+        if (
+            stack.length === 0 ||
+            time > stack[stack.length - 1]
+        ) {
+            stack.push(time);
         }
     }
+
     return stack.length;
 };
