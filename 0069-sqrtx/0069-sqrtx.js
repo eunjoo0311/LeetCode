88b596b84332d2f82b56f90a7c16b9876b1281cd
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-24 20:45:57 +0900

    Time: 1 ms (74.55%), Space: 55 MB (93.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let left = 0;
+    let right = x
+
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        const square = mid * mid
+        if(square === x) {
+            return mid
+        } else if(square < x) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return right
+};
