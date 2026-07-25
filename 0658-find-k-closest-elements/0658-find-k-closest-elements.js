31cf13c37c620669faf7190f74bef0fc9bd40aa2
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-07-25 09:38:09 +0900

    Time: 0 ms (100%), Space: 60.7 MB (80.37%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.js
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -5,19 +5,18 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let left = 0
+    let left = 0;
     let right = arr.length - 1
 
-    while(right - left + 1 > k) {
-        const leftDistance = Math.abs(arr[left] - x)
-        const rightDistance = Math.abs(arr[right] - x)
+    while(left < right) {
+        const mid = Math.floor((left + right) / 2)
 
-        if(leftDistance > rightDistance) {
-            left++
+        if(x - arr[mid] > arr[mid + k] - x) {
+            left = mid + 1
         } else {
-            right--
+            right = mid
         }
     }
 
-    return arr.slice(left, right + 1)
+    return arr.slice(left, left + k)
 };
