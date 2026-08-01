0dff8dfd9a6c550fbaf2c95d327f8836b7e8d27a
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-01 21:31:56 +0900

    Time: 2 ms (44.96%), Space: 59 MB (82.28%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    function getDepth(node) {
+        if(node === null) return 0 
+
+        const leftDepth = getDepth(node.left)
+
+        if(leftDepth === -1) {
+            return -1
+        }
+        const rightDepth = getDepth(node.right)
+
+        if(rightDepth === -1) return -1
+
+        if(Math.abs(leftDepth - rightDepth) > 1) return -1
+
+        return Math.max(leftDepth, rightDepth) + 1
+    }
+
+    return getDepth(root) !== -1
+};
