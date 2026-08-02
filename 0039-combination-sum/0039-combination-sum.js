f19b992e9882d89592e83ebdb0dfe81086123add
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-02 15:34:27 +0900

    Time: 2 ms (81.18%), Space: 60.4 MB (32.17%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -10,19 +10,16 @@ var combinationSum = function(candidates, target) {
         if(sum === target) {
             return answer.push([...path])
         }
-        if(sum > target) {
-            return 
-        }
+
+        if(sum > target) return
 
         for(let i = start; i < candidates.length; i++) {
             path.push(candidates[i])
             dfs(i, path, sum + candidates[i])
             path.pop()
         }
-
     }
 
     dfs(0, [], 0)
-
     return answer
 };
