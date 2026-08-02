ca3bdd7a22f93bc6848ed344c25bb1b1cabce3a8
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-02 18:25:02 +0900

    Time: 290 ms (79.65%), Space: 55.8 MB (39.39%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -3,37 +3,32 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
+var exist = function (board, word) {
+    // dfs
     const rows = board.length
     const cols = board[0].length
 
     const dfs = (row, col, index) => {
-        if(index === word.length) {
+        if (index === word.length) {
             return true
         }
-
-        if(row < 0 || row >= rows || col < 0 || col >= cols) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
             return false
         }
-
-        if(board[row][col] !== word[index]) return false
+        if (board[row][col] !== word[index]) return false
 
         const temp = board[row][col]
         board[row][col] = '#'
-        
-        const found = dfs(row + 1, col, index + 1) ||
-        dfs(row - 1, col, index + 1) ||
-        dfs(row, col - 1, index + 1) ||
-        dfs(row, col + 1, index + 1) 
+
+        const found = dfs(row + 1, col, index + 1) || dfs(row - 1, col, index + 1) || dfs(row, col + 1, index + 1) || dfs(row, col - 1, index + 1)
 
         board[row][col] = temp
-        
         return found
     }
 
-    for(let row = 0; row < rows; row++) {
-        for(let col = 0; col < cols; col++) {
-            if(dfs(row, col, 0)) {
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (dfs(row, col, 0)) {
                 return true
             }
         }
