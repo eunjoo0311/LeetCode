4f83ebd2ff23260c4301b6e37f4be638c777a81a
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-03 20:43:53 +0900

    Time: 0 ms (100%), Space: 57 MB (94.77%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,29 +11,24 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if(root === null) return []
-
-    const answer = []
+    // bfs
     const queue = [root]
+    const answer = []
 
     while(queue.length > 0) {
         const size = queue.length
         const level = []
-
         for(let i = 0; i < size; i++) {
             const node = queue.shift()
 
+            if(node === null) return []
+            
             level.push(node.val)
 
-            if(node.left !== null) {
-                queue.push(node.left)
-            }
-            if(node.right !== null) {
-                queue.push(node.right)
-            }
+            if(node.left !== null) queue.push(node.left)
+            if(node.right !== null) queue.push(node.right)
         }
         answer.push(level)
     }
-
     return answer
 };
