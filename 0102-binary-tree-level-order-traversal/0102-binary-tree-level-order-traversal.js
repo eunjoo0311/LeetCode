cbcb29ca1d7638867490d49ccb86f4fc21d8aa69
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-03 20:52:26 +0900

    Time: 0 ms (100%), Space: 57.2 MB (92.22%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -12,23 +12,40 @@
  */
 var levelOrder = function(root) {
     // bfs
-    const queue = [root]
+    // const queue = [root]
+    // const answer = []
+
+    // while(queue.length > 0) {
+    //     const size = queue.length
+    //     const level = []
+    //     for(let i = 0; i < size; i++) {
+    //         const node = queue.shift()
+    //         if(node === null) return []
+            
+    //         level.push(node.val)
+    //         if(node.left !== null) queue.push(node.left)
+    //         if(node.right !== null) queue.push(node.right)
+    //     }
+    //     answer.push(level)
+    // }
+    // return answer
+
+    // dfs
     const answer = []
 
-    while(queue.length > 0) {
-        const size = queue.length
-        const level = []
-        for(let i = 0; i < size; i++) {
-            const node = queue.shift()
+    const dfs = (node, depth) => {
+        if(node === null) return []
 
-            if(node === null) return []
-            
-            level.push(node.val)
-
-            if(node.left !== null) queue.push(node.left)
-            if(node.right !== null) queue.push(node.right)
+        if(answer[depth] === undefined) {
+            answer[depth] = []
         }
-        answer.push(level)
+
+        answer[depth].push(node.val)
+
+        if(node.left !== null) dfs(node.left, depth + 1)
+        if(node.right !== null) dfs(node.right, depth + 1)
     }
+
+    dfs(root, 0)
     return answer
 };
