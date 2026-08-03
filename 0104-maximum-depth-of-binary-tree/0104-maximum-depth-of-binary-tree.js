2c829acd2856f8739ec4987f9327c9ca4c4154d6
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-03 20:14:22 +0900

    Time: 1 ms (52.12%), Space: 62.3 MB (16.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -12,13 +12,30 @@
  */
 var maxDepth = function(root) {
     // dfs
+    // let depth = 0
+    // const dfs = (node, index) => {
+    //     if(node === null) return 
+    //     depth = Math.max(depth,)
+    //     if(node.left !== null) dfs(node.left, index + 1)
+    //     if(node.right !== null) dfs(node.right, index + 1)
+    // }
+    // dfs(root, 1)
+    // return depth
+
+    // bfs
+    if(root === null) return 0
+    const queue = [root]
     let depth = 0
-    const dfs = (node, index) => {
-        if(node === null) return 
-        depth = Math.max(depth, index)
-        if(node.left !== null) dfs(node.left, index + 1)
-        if(node.right !== null) dfs(node.right, index + 1)
+
+    while(queue.length > 0) {
+        const size = queue.length
+
+        for(let i = 0; i < size; i++) {
+            const node = queue.shift()
+            if(node.left !== null) queue.push(node.left)
+            if(node.right !== null) queue.push(node.right)
+        }
+        depth++
     }
-    dfs(root, 1)
     return depth
 };
