ca903878a8e297efc6e1e310059033f8c606e7e4
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-04 23:55:18 +0900

    Time: 2 ms (91.58%), Space: 55.7 MB (97.69%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+    // dfs
+    const rows = grid.length
+    const cols = grid[0].length
+
+    let area = 0
+
+    const dfs = (row, col) => {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return 0
+        if (grid[row][col] === 0) return 0
+
+        grid[row][col] = 0
+
+        return dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1) + 1
+    }
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === 1) {
+                area = Math.max(area, dfs(row, col))
+            }
+        }
+    }
+
+    return area
+};
