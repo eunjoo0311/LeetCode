49e7e5e4571a099ec1a6cef4f9c65e551ffce27d
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-04 22:35:32 +0900

    Time: 79 ms (22.07%), Space: 63.2 MB (33.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,33 +2,76 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
+var numIslands = function (grid) {
     // dfs
+    // const rows = grid.length
+    // const cols = grid[0].length
+
+    // let count = 0
+
+    // const dfs = (row, col) => {
+    //     if(row < 0 || row >= rows || col < 0 || col >= cols) return
+    //     if(grid[row][col] === '0') return
+
+    //     grid[row][col] = '0'
+
+    //     dfs(row + 1, col)
+    //     dfs(row - 1, col)
+    //     dfs(row, col + 1)
+    //     dfs(row, col - 1)
+    // }
+
+    // for(let row = 0; row < rows; row++) {
+    //     for(let col = 0; col < cols; col++) {
+    //         if(grid[row][col] === '1') {
+    //             count++
+    //             dfs(row, col)
+    //         }
+    //     }
+    // }
+
+    // return count
+
+    // bfs
     const rows = grid.length
     const cols = grid[0].length
 
     let count = 0
 
-    const dfs = (row, col) => {
-        if(row < 0 || row >= rows || col < 0 || col >= cols) return
-        if(grid[row][col] === '0') return
-
-        grid[row][col] = '0'
-
-        dfs(row + 1, col)
-        dfs(row - 1, col)
-        dfs(row, col + 1)
-        dfs(row, col - 1)
-    }
-
-    for(let row = 0; row < rows; row++) {
-        for(let col = 0; col < cols; col++) {
-            if(grid[row][col] === '1') {
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === '1') {
                 count++
-                dfs(row, col)
+
+                grid[row][col] = '0'
+                const queue = [[row, col]]
+
+                while (queue.length > 0) {
+                    const [currentRow, currentCol] = queue.shift()
+
+                    if (currentRow - 1 >= 0 && grid[currentRow - 1][currentCol] === '1') {
+                        grid[currentRow - 1][currentCol] = '0'
+                        queue.push([currentRow - 1, currentCol])
+                    }
+
+                    if (currentRow + 1 < rows && grid[currentRow + 1][currentCol] === '1') {
+                        grid[currentRow + 1][currentCol] = '0'
+                        queue.push([currentRow + 1, currentCol])
+                    }
+
+
+                    if (currentCol - 1 >= 0 && grid[currentRow][currentCol - 1] === '1') {
+                        grid[currentRow][currentCol - 1] = '0'
+                        queue.push([currentRow, currentCol - 1])
+                    }
+
+                    if (currentCol + 1 < cols && grid[currentRow][currentCol + 1] === '1') {
+                        grid[currentRow][currentCol + 1] = '0'
+                        queue.push([currentRow, currentCol + 1])
+                    }
+                }
             }
         }
     }
-
     return count
 };
