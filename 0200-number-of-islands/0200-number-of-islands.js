69bb7dc16c325304012b8a400ce35cf5db119af4
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-04 22:16:51 +0900

    Time: 48 ms (96.17%), Space: 57.5 MB (92.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,15 +2,16 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function (grid) {
+var numIslands = function(grid) {
+    // dfs
     const rows = grid.length
     const cols = grid[0].length
 
     let count = 0
 
     const dfs = (row, col) => {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) return
-        if (grid[row][col] === '0') return
+        if(row < 0 || row >= rows || col < 0 || col >= cols) return
+        if(grid[row][col] === '0') return
 
         grid[row][col] = '0'
 
@@ -20,13 +21,14 @@ var numIslands = function (grid) {
         dfs(row, col - 1)
     }
 
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (grid[row][col] === '1') {
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            if(grid[row][col] === '1') {
                 count++
                 dfs(row, col)
             }
         }
     }
+
     return count
 };
