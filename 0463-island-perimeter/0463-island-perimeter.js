bedb85406bd44889ca7e254061c3738eb56e6d76
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-04 22:13:51 +0900

    Time: 21 ms (19.47%), Space: 66.4 MB (12.39%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.js
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -4,26 +4,77 @@
  */
 var islandPerimeter = function (grid) {
     // dfs
+    // const rows = grid.length
+    // const cols = grid[0].length
+
+    // const dfs = (row, col) => {
+    //     if (row < 0 || row >= rows || col < 0 || col >= cols) return 1
+    //     if (grid[row][col] === 0) return 1
+    //     if (grid[row][col] === -1) return 0
+
+    //     grid[row][col] = -1
+
+    //     return dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
+    // }
+
+    // for (let row = 0; row < rows; row++) {
+    //     for (let col = 0; col < cols; col++) {
+    //         if (grid[row][col] === 1) {
+    //             return dfs(row, col)
+    //         }
+    //     }
+    // }
+
+    // return 0
+
+    // bfs
+
     const rows = grid.length
     const cols = grid[0].length
 
-    const dfs = (row, col) => {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) return 1
-        if (grid[row][col] === 0) return 1
-        if (grid[row][col] === -1) return 0
-
-        grid[row][col] = -1
-
-        return dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
-    }
+    const queue = []
+    let perimeter = 0
 
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < cols; col++) {
             if (grid[row][col] === 1) {
-                return dfs(row, col)
+                queue.push([row, col])
+                grid[row][col] = -1
+                break
             }
         }
     }
 
-    return 0
+    while (queue.length > 0) {
+        const [row, col] = queue.shift()
+
+        if (row - 1 < 0 || grid[row - 1][col] === 0) {
+            perimeter++
+        } else if (grid[row - 1][col] === 1) {
+            queue.push([row - 1, col])
+            grid[row - 1][col] = -1
+        }
+
+        if (col + 1 >= cols || grid[row][col + 1] === 0) {
+            perimeter++
+        } else if (grid[row][col + 1] === 1) {
+            queue.push([row, col + 1])
+            grid[row][col + 1] = -1
+        }
+
+        if (col - 1 < 0 || grid[row][col - 1] === 0) {
+            perimeter++
+        } else if (grid[row][col - 1] === 1) {
+            queue.push([row, col - 1])
+            grid[row][col - 1] = -1
+        }
+
+        if (row + 1 >= rows || grid[row + 1][col] === 0) {
+            perimeter++
+        } else if (grid[row + 1][col] === 1) {
+            queue.push([row + 1, col])
+            grid[row + 1][col] = -1
+        }
+    }
+    return perimeter
 };
