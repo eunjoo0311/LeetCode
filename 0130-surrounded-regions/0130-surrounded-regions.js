ffd2cec3eca5e80920ba0a5fbe6d50b6c6121c1b
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-07 22:56:43 +0900

    Time: 5 ms (63.39%), Space: 60.5 MB (34.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.js
+++ 0130-surrounded-regions/0130-surrounded-regions.js
@@ -3,28 +3,83 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function (board) {
+    // const rows = board.length
+    // const cols = board[0].length
+
+    // const dfs = (row, col) => {
+    //     if (row < 0 || row >= rows || col < 0 || col >= cols) return
+    //     if (board[row][col] !== 'O') return
+    //     board[row][col] = '#'
+
+    //     dfs(row + 1, col)
+    //     dfs(row - 1, col)
+    //     dfs(row, col + 1)
+    //     dfs(row, col - 1)
+    // }
+
+    // for (let row = 0; row < rows; row++) {
+    //     dfs(row, 0)
+    //     dfs(row, cols - 1)
+    // }
+
+    // for (let col = 0; col < cols; col++) {
+    //     dfs(0, col)
+    //     dfs(rows - 1, col)
+    // }
+
+    // for (let row = 0; row < rows; row++) {
+    //     for (let col = 0; col < cols; col++) {
+    //         if (board[row][col] === 'O') {
+    //             board[row][col] = 'X'
+    //         } else if (board[row][col] === '#') {
+    //             board[row][col] = 'O'
+    //         }
+    //     }
+    // }
+
     const rows = board.length
     const cols = board[0].length
 
-    const dfs = (row, col) => {
-        if (row < 0 || row >= rows || col < 0 || col >= cols) return
-        if (board[row][col] !== 'O') return
-        board[row][col] = '#'
+    const bfs = (startRow, startCol) => {
+        if (board[startRow][startCol] !== 'O') return
+        const queue = [[startRow, startCol]]
+        board[startRow][startCol] = '#'
 
-        dfs(row + 1, col)
-        dfs(row - 1, col)
-        dfs(row, col + 1)
-        dfs(row, col - 1)
+        while (queue.length > 0) {
+            const [row, col] = queue.shift()
+
+            if (row - 1 >= 0 && board[row - 1][col] === 'O') {
+                board[row - 1][col] = '#'
+                queue.push([row - 1, col])
+            }
+
+            if (row + 1 < rows && board[row + 1][col] === 'O') {
+                board[row + 1][col] = '#'
+                queue.push([row + 1, col])
+            }
+
+            if (col - 1 >= 0 && board[row][col - 1] === 'O') {
+                board[row][col - 1] = '#'
+                queue.push([row, col - 1])
+            }
+
+
+            if (col + 1 < cols && board[row][col + 1] === 'O') {
+                board[row][col + 1] = '#'
+                queue.push([row, col + 1])
+            }
+        }
+
     }
 
     for (let row = 0; row < rows; row++) {
-        dfs(row, 0)
-        dfs(row, cols - 1)
+        bfs(row, 0)
+        bfs(row, cols - 1)
     }
 
     for (let col = 0; col < cols; col++) {
-        dfs(0, col)
-        dfs(rows - 1, col)
+        bfs(0, col)
+        bfs(rows - 1, col)
     }
 
     for (let row = 0; row < rows; row++) {
