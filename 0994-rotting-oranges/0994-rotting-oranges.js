5c2faccb42b9f367a9fc92c317bc10c7a6d01ed5
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-07 22:39:16 +0900

    Time: 9 ms (53.39%), Space: 59.1 MB (38.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -3,55 +3,100 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    // const rows = grid.length
+    // const cols = grid[0].length
+
+    // const queue = []
+    // let fresh = 0;
+    // let minutes = 0
+
+    // for (let row = 0; row < rows; row++) {
+    //     for (let col = 0; col < cols; col++) {
+    //         if (grid[row][col] === 2) {
+    //             queue.push([row, col])
+    //         }
+    //         else if (grid[row][col] === 1) {
+    //             fresh++
+    //         }
+    //     }
+    // }
+
+    // while (queue.length > 0 && fresh > 0) {
+    //     const size = queue.length
+
+    //     for (let i = 0; i < size; i++) {
+    //         const [row, col] = queue.shift()
+
+    //         if (row - 1 >= 0 && grid[row - 1][col] === 1) {
+    //             grid[row - 1][col] = 2
+    //             queue.push([row - 1, col])
+    //             fresh--
+    //         }
+
+    //         if (row + 1 < rows && grid[row + 1][col] === 1) {
+    //             grid[row + 1][col] = 2
+    //             queue.push([row + 1, col])
+    //             fresh--
+    //         }
+
+    //         if (col - 1 >= 0 && grid[row][col - 1] === 1) {
+    //             grid[row][col - 1] = 2
+    //             queue.push([row, col - 1])
+    //             fresh--
+    //         }
+
+    //         if (col + 1 < cols && grid[row][col + 1] === 1) {
+    //             grid[row][col + 1] = 2
+    //             queue.push([row, col + 1])
+    //             fresh--
+    //         }
+    //     }
+    //     minutes++
+    // }
+    // return fresh === 0 ? minutes : -1
+
     const rows = grid.length
     const cols = grid[0].length
 
-    const queue = []
-    let fresh = 0;
-    let minutes = 0
+    const times = Array.from({ length: rows }, () => Array(cols).fill(Infinity))
+    const dfs = (row, col, time) => {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) return
+        if (grid[row][col] !== 1) return
+
+        if (times[row][col] <= time) return
+
+        times[row][col] = time
+
+        dfs(row + 1, col, time + 1)
+        dfs(row - 1, col, time + 1)
+        dfs(row, col + 1, time + 1)
+        dfs(row, col - 1, time + 1)
+
+    }
 
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < cols; col++) {
             if (grid[row][col] === 2) {
-                queue.push([row, col])
-            }
-            else if (grid[row][col] === 1) {
-                fresh++
+                dfs(row - 1, col, 1)
+                dfs(row + 1, col, 1)
+                dfs(row, col - 1, 1)
+                dfs(row, col + 1, 1)
             }
         }
     }
 
-    while (queue.length > 0 && fresh > 0) {
-        const size = queue.length
+    let answer = 0
 
-        for (let i = 0; i < size; i++) {
-            const [row, col] = queue.shift()
-
-            if (row - 1 >= 0 && grid[row - 1][col] === 1) {
-                grid[row - 1][col] = 2
-                queue.push([row - 1, col])
-                fresh--
-            }
-
-            if (row + 1 < rows && grid[row + 1][col] === 1) {
-                grid[row + 1][col] = 2
-                queue.push([row + 1, col])
-                fresh--
-            }
-
-            if (col - 1 >= 0 && grid[row][col - 1] === 1) {
-                grid[row][col - 1] = 2
-                queue.push([row, col - 1])
-                fresh--
-            }
-
-            if (col + 1 < cols && grid[row][col + 1] === 1) {
-                grid[row][col + 1] = 2
-                queue.push([row, col + 1])
-                fresh--
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === 1) {
+                if (times[row][col] === Infinity) {
+                    return -1
+                }
+                answer = Math.max(answer, times[row][col])
             }
         }
-        minutes++
     }
-    return fresh === 0 ? minutes : -1
+
+    return answer
 };
