b8ca5e60de5576a7a718b6c87eaf3d7c344b38fc
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 18:49:21 +0900

    Time: 9 ms (73.48%), Space: 61.7 MB (58.12%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    const graph = Array.from({length : numCourses}, () => [])
+
+    for(let [a, b] of prerequisites) {
+        graph[b].push(a)
+    }
+
+    const visited = Array(numCourses).fill(0)
+    const answer = []
+
+    const dfs = (course) => {
+        if(visited[course] === 1) return false
+        if(visited[course] === 2) return true
+
+        visited[course] = 1
+
+        for(const next of graph[course]) {
+            if(!dfs(next)) {
+                return false
+            }
+        }
+        visited[course] = 2
+        answer.push(course)
+        return true
+    }
+
+    for(let i = 0; i < numCourses; i++) {
+        if(!dfs(i)) {
+            return []
+        }
+    }
+
+    return answer.reverse()
+};
