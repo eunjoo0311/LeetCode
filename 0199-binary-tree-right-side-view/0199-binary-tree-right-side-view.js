cb2d6dfd1d3258c5e1d56f171e35a591a6f47bcc
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 13:38:14 +0900

    Time: 0 ms (100%), Space: 55 MB (80.98%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(root === null) return []
+    const queue = [root]
+    const answer = []
+
+    while(queue.length > 0) {
+        const size = queue.length
+        const level = []
+
+        for(let i = 0; i < size; i++) {
+            const node = queue.shift()
+
+            level.push(node.val)
+
+            if(node.left !== null) queue.push(node.left)
+            if(node.right !== null) queue.push(node.right)
+        }
+        answer.push(level[level.length -1])
+    }
+    return answer
+};
