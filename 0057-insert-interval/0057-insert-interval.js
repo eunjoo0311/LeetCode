f89149c67529fa4158768bfe055672c9106640ce
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 19:04:01 +0900

    Time: 3 ms (61.02%), Space: 59.2 MB (21.52%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,25 +4,23 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    // 담아둘 배열, index, 마지막 값 잡기
     const answer = []
-    let i = 0;
+
+    let i = 0
     let n = intervals.length
 
-    // 합치기 전 배열 푸시하기
     while(i < n && intervals[i][1] < newInterval[0]) {
         answer.push(intervals[i])
         i++
     }
-    // 겹치기 
+
     while(i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], intervals[i][0])
-        newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+        newInterval[0] = Math.min(intervals[i][0], newInterval[0])
+        newInterval[1] = Math.max(intervals[i][1], newInterval[1])
         i++
     }
     answer.push(newInterval)
 
-    // 후 부분 배열 푸시하기
     while(i < n) {
         answer.push(intervals[i])
         i++
