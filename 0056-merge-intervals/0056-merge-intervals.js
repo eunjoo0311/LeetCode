a6359ad7c6519da73aa29d4ae0cef1828308f852
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 19:12:31 +0900

    Time: 8 ms (67.78%), Space: 66 MB (28.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,28 +4,18 @@
  */
 var merge = function (intervals) {
     intervals.sort((a, b) => a[0] - b[0])
+
     const answer = []
 
-    // for문으로 순회
-    // 현재 값 시작과 끝 지정
-    // if문으로 첫번째는 그냥 푸시하기
-    // 마지막 값 가져오기
-    // 비교해서 합치거나 아니라면 푸시하기
-
-    for (let interval of intervals) {
-        const curStart = interval[0]
-        const curEnd = interval[1]
-
+    for (const interval of intervals) {
         if (answer.length === 0) {
             answer.push(interval)
-            continue
         }
 
         const last = answer[answer.length - 1]
-        const lastEnd = last[1]
 
-        if (lastEnd >= curStart) {
-            last[1] = Math.max(lastEnd, curEnd)
+        if(last[1] >= interval[0]) {
+            last[1] = Math.max(interval[1], last[1])
         } else {
             answer.push(interval)
         }
