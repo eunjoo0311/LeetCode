d4ca9b71f5e0535cb7a3fc0119655b9cc00b7ae0
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 19:17:27 +0900

    Time: 78 ms (56.73%), Space: 86.5 MB (53.4%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    // 빨리 끝나는 지점 기준으로 정렬
     intervals.sort((a,b) => a[1] - b[1])
 
-    // count 변수랑, 전꺼의 끝값 저장할 변수
-    let count = 0;
+    let count = 0
     let prevEnd = intervals[0][1]
-
+    
     for(let i = 1; i < intervals.length; i++) {
-        const curStart = intervals[i][0]
-        const curEnd = intervals[i][1]
+        let curStart = intervals[i][0]
+        let curEnd = intervals[i][1]
 
-        if(curStart < prevEnd) {
+        if(prevEnd > curStart) {
             count++
         } else {
             prevEnd = curEnd
@@ -22,5 +20,4 @@ var eraseOverlapIntervals = function(intervals) {
     }
 
     return count
-
 };
