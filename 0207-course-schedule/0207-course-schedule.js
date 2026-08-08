ee215cf581f83355f83a967e9fe6ad196dd9b793
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 18:31:44 +0900

    Time: 11 ms (74.86%), Space: 60.9 MB (83.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -3,24 +3,25 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function (numCourses, prerequisites) {
-    const graph = Array.from({ length: numCourses }, () => [])
+var canFinish = function(numCourses, prerequisites) {
+    const graph = Array.from({length : numCourses}, () => [])
+    // 선수 과목을 담을 배열 만들기
 
-    for (let [a, b] of prerequisites) {
+    for(let [a, b] of prerequisites) {
         graph[b].push(a)
     }
+    // 배열에 담기
 
     const visited = Array(numCourses).fill(0)
+    // 방문 여부 처리
 
     const dfs = (course) => {
-        if (visited[course] === 1) {
-            return false
-        }
-        if (visited[course] === 2) {
-            return true
-        }
+        if(visited[course] === 1) return false
+        // 이미 방문 중이라면
+        if(visited[course] === 2) return true
+        // 방문 끝이라면
 
-        visited[course] = 1;
+        visited[course] = 1
 
         for(let next of graph[course]) {
             if(!dfs(next)) {
@@ -31,11 +32,10 @@ var canFinish = function (numCourses, prerequisites) {
         return true
     }
 
-    for (let i = 0; i < numCourses; i++) {
-        if (!dfs(i)) {
+    for(let i = 0; i < numCourses; i++) {
+        if(!dfs(i)) {
             return false
         }
     }
-
     return true
 };
