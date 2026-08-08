cf7273eae64a6283d7cfb58fbd620a0cce6d6124
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-08 13:25:19 +0900

    Time: 18 ms (65.6%), Space: 54.2 MB (98.22%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -5,20 +5,25 @@
  */
 var characterReplacement = function(s, k) {
     const map = {}
+    let maxCount = 0
 
     let left = 0
-    let maxCount = 0;
-    let answer = 0
+    // let right = s.length - 1
+
+    let answer= 0 
+
 
     for(let right = 0; right < s.length; right++) {
         map[s[right]] = (map[s[right]] || 0) + 1
-        maxCount = Math.max(maxCount, map[s[right]])
+        maxCount = Math.max(map[s[right]], maxCount)
 
         while(right - left + 1 - maxCount > k) {
             map[s[left]]--
             left++
         }
-        answer = Math.max(answer, right- left + 1)
+
+        answer = Math.max(answer, right - left + 1)
     }
-    return  answer
+
+    return answer
 };
