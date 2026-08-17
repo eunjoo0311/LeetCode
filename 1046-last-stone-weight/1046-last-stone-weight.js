82373d34a23b0e96ed4adaa93ae11d46f28ffb61
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-17 17:50:42 +0900

    Time: 4 ms (57.86%), Space: 55.9 MB (42.45%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -1,8 +1,9 @@
-class stoneHeap {
+class sHeap {
     constructor(compare) {
         this.heap = []
         this.compare = compare
     }
+
     size() {
         return this.heap.length
     }
@@ -13,9 +14,24 @@ class stoneHeap {
         this.heap.push(value)
         this.bubbleUp()
     }
+
+    bubbleUp() {
+        let index = this.size() - 1
+
+        while(index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2)
+            
+            if(this.compare(this.heap[parentIndex], this.heap[index])) {
+                break
+            }
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
+
+            index = parentIndex
+        }
+    }
     pop() {
-        if (this.heap.length === 0) return null
-        if (this.heap.length === 1) return this.heap.pop()
+        if(this.size() === 0 ) return null
+        if(this.size() === 1 ) return this.heap.pop()
 
         const top = this.heap[0]
         this.heap[0] = this.heap.pop()
@@ -23,65 +39,48 @@ class stoneHeap {
 
         return top
     }
-    bubbleUp() {
-        let index = this.size() - 1
-
-        while (index > 0) {
-            let parentIndex = Math.floor((index - 1) / 2)
-
-            if (this.compare(this.heap[parentIndex], this.heap[index])) {
-                break
-            }
-            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
-            index = parentIndex
-        }
-    }
-
     bubbleDown() {
         let index = 0
-        while (true) {
+        while(true) {
             let targetIndex = index
 
             let leftIndex = index * 2 + 1
             let rightIndex = index * 2 + 2
-
-            if (leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
+            
+            if(leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
                 targetIndex = leftIndex
             }
-            if (rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
+
+            if(rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
                 targetIndex = rightIndex
             }
+            if(targetIndex === index) break
 
-            if (index === targetIndex) {
-                break
-            }
-
-            [this.heap[index], this.heap[targetIndex]] = [this.heap[targetIndex], this.heap[index]]
-
+            [this.heap[targetIndex], this.heap[index]] = [this.heap[index], this.heap[targetIndex]]
+            
             index = targetIndex
         }
     }
-
 }
+
 /**
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function (stones) {
-    const maxHeap = new stoneHeap((a, b) => a > b)
+var lastStoneWeight = function(stones) {
+    const heap = new sHeap((a,b) => a >= b)
 
-    for(let stone of stones) {
-        maxHeap.push(stone)
+    for(const stone of stones) {
+        heap.push(stone)
     }
 
-    while(maxHeap.size() > 1) {
-        const first = maxHeap.pop()
-        const second = maxHeap.pop()
+    while(heap.size() > 1) {
+        const y = heap.pop()
+        const x = heap.pop()
 
-        if(first !== second) {
-            maxHeap.push(first - second)
+        if(x !== y) {
+            heap.push(y - x)
         }
     }
-
-    return maxHeap.size() === 0 ? 0 : maxHeap.pop()
+    return heap.peek() ?? 0
 };
