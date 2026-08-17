d7b6f6a1119a4c227cac224f9b630076556be2f7
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-17 18:27:45 +0900

    Time: 57 ms (76.53%), Space: 75.4 MB (19.22%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -13,9 +13,22 @@ class kHeap {
         this.heap.push(value)
         this.bubbleUp()
     }
+    bubbleUp() {
+        let index = this.size() - 1
+
+        while(index > 0) {
+            let parentIndex = Math.floor((index - 1)/ 2)
+
+            if(this.compare(this.heap[parentIndex], this.heap[index])) {
+                break
+            }
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
+            index = parentIndex
+        }
+    }
     pop() {
-        if (this.size() === 0) return null
-        if (this.size() === 1) return this.heap.pop()
+        if(this.size() === 0) return null
+        if(this.size() === 1) return this.heap.pop()
 
         const top = this.heap[0]
         this.heap[0] = this.heap.pop()
@@ -23,43 +36,21 @@ class kHeap {
 
         return top
     }
-    bubbleUp() {
-        let index = this.size() - 1
-
-        while (index > 0) {
-            let parentIndex = Math.floor((index - 1) / 2)
-
-            if (this.compare(this.heap[parentIndex], this.heap[index])) {
-                break
-            }
-            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
-
-            index = parentIndex
-        }
-    }
-
     bubbleDown() {
         let index = 0
-
-        while (true) {
+        while(true) {
             let targetIndex = index
-
             let leftIndex = index * 2 + 1
             let rightIndex = index * 2 + 2
 
-            if (leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
+            if(leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
                 targetIndex = leftIndex
             }
-            if (rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
+            if(rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
                 targetIndex = rightIndex
             }
-
-            if (targetIndex === index) {
-                break
-            }
-
-            [this.heap[index], this.heap[targetIndex]] = [this.heap[targetIndex], this.heap[index]]
-
+            if(targetIndex === index) break
+            [this.heap[targetIndex], this.heap[index]] = [this.heap[index], this.heap[targetIndex]]
             index = targetIndex
         }
     }
@@ -71,14 +62,15 @@ class kHeap {
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-    const maxHeap = new kHeap((a, b) => a < b)
+    const heap = new kHeap((a, b) => a <= b)
 
-    for(let num of nums) {
-        maxHeap.push(num)
+    for (const num of nums) {
+        heap.push(num)
 
-        if(maxHeap.size() > k) {
-            maxHeap.pop()
+        if (heap.size() > k) {
+            heap.pop()
         }
     }
-    return maxHeap.peek()
+
+    return heap.peek()
 };
