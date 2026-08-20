9045502d70247851630be7690c284faa4b56e4d1
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-20 23:45:38 +0900

    Time: 68 ms (65.09%), Space: 61.3 MB (98.69%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.js
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -5,16 +5,18 @@
  */
 var numRescueBoats = function (people, limit) {
     people.sort((a, b) => a - b)
+
     let left = 0;
     let right = people.length - 1
-    let boats = 0;
+    let boats = 0
 
-    while (left <= right) {
-        if (people[left] + people[right] <= limit) {
+    while(left <= right) {
+        if(people[left] + people[right] <= limit) {
             left++
         }
         right--
         boats++
     }
+
     return boats
 };
