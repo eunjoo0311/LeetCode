def456ef0a0b3bb15a5b4f875f2b08fe015d812d
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-21 22:59:55 +0900

    Time: 4 ms (50%), Space: 56.5 MB (26%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.js
+++ 1405-longest-happy-string/1405-longest-happy-string.js
@@ -9,8 +9,8 @@ class hHeap {
     peek() {
         return this.heap[0]
     }
-    push(value) {
-        this.heap.push(value)
+    push(val) {
+        this.heap.push(val)
         this.bubbleUp()
     }
     pop() {
@@ -32,39 +32,32 @@ class hHeap {
             if (this.compare(this.heap[parentIndex], this.heap[index])) {
                 break
             }
-
             [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
-
             index = parentIndex
         }
     }
-
     bubbleDown() {
         let index = 0
 
         while (true) {
             let targetIndex = index
-
             let leftIndex = index * 2 + 1
             let rightIndex = index * 2 + 2
 
             if (leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
                 targetIndex = leftIndex
             }
-
             if (rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
                 targetIndex = rightIndex
             }
-
             if (targetIndex === index) {
                 break
             }
-
-            [this.heap[index], this.heap[targetIndex]] = [this.heap[targetIndex], this.heap[index]]
-
+            [this.heap[targetIndex], this.heap[index]] = [this.heap[index], this.heap[targetIndex]]
             index = targetIndex
         }
     }
+
 }
 /**
  * @param {number} a
@@ -73,37 +66,32 @@ class hHeap {
  * @return {string}
  */
 var longestDiverseString = function (a, b, c) {
-    const heap = new hHeap((a,b) => a[0] >= b[0])
+    const heap = new hHeap((a, b) => a[0] >= b[0])
 
-    if(a > 0) heap.push([a, 'a'])
-    if(b > 0) heap.push([b, 'b'])
-    if(c > 0) heap.push([c, 'c'])
+    if (a > 0) heap.push([a, 'a'])
+    if (b > 0) heap.push([b, 'b'])
+    if (c > 0) heap.push([c, 'c'])
 
     let answer = ""
 
-    while(heap.size() > 0) {
+    while (heap.size() > 0) {
         let [count, char] = heap.pop()
-
-        if(answer.length >= 2 && answer[answer.length - 1] === char && answer[answer.length - 2] === char) {
-            if(heap.size() === 0) {
+        const len = answer.length
+        if (len >= 2 && answer[len - 2] === char && answer[len - 1] === char) {
+            if (heap.size() === 0) {
                 break
             }
-
             let [secondCount, secondChar] = heap.pop()
-
             answer += secondChar
-            secondCount--
-
-            if(secondCount > 0) {
-                heap.push([secondCount, secondChar])
+            if (secondCount - 1 > 0) {
+                heap.push([secondCount - 1, secondChar])
             }
             heap.push([count, char])
         } else {
             answer += char
-            count--
 
-            if(count > 0) {
-                heap.push([count, char])
+            if (count - 1 > 0) {
+                heap.push([count - 1, char])
             }
         }
     }
