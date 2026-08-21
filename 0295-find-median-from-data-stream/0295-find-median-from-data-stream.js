c13a98d9beebd294121d58c0cd7722613593c044
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-21 23:07:15 +0900

    Time: 247 ms (21.55%), Space: 93.3 MB (13.26%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
@@ -4,18 +4,18 @@ class MedianHeap {
         this.compare = compare
     }
     size() {
-       return this.heap.length
+        return this.heap.length
     }
     peek() {
-       return this.heap[0]
+        return this.heap[0]
     }
     push(value) {
         this.heap.push(value)
         this.bubbleUp()
     }
     pop() {
-        if(this.size() === 0) return null
-        if(this.size() === 1) return this.heap.pop()
+        if (this.size() === 0) return null
+        if (this.size() === 1) return this.heap.pop()
 
         const top = this.heap[0]
         this.heap[0] = this.heap.pop()
@@ -26,10 +26,10 @@ class MedianHeap {
     bubbleUp() {
         let index = this.size() - 1
 
-        while(index > 0) {
-            let parentIndex = Math.floor((index - 1) /2)
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2)
 
-            if(this.compare(this.heap[parentIndex], this.heap[index])) {
+            if (this.compare(this.heap[parentIndex], this.heap[index])) {
                 break
             }
 
@@ -42,42 +42,41 @@ class MedianHeap {
     bubbleDown() {
         let index = 0
 
-        while(true) {
+        while (true) {
             let targetIndex = index
 
             let leftIndex = index * 2 + 1
             let rightIndex = index * 2 + 2
 
-            if(leftIndex < this.size() &&!this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
+            if (leftIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[leftIndex])) {
                 targetIndex = leftIndex
             }
 
-            if(rightIndex < this.size() &&!this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
+            if (rightIndex < this.size() && !this.compare(this.heap[targetIndex], this.heap[rightIndex])) {
                 targetIndex = rightIndex
             }
 
-            if(targetIndex === index) {
+            if (targetIndex === index) {
                 break
             }
 
             [this.heap[index], this.heap[targetIndex]] = [this.heap[targetIndex], this.heap[index]]
-            
+
             index = targetIndex
         }
     }
 }
-var MedianFinder = function() {
-    this.maxHeap = new MedianHeap((a,b) => a > b)
-    this.minHeap = new MedianHeap((a,b) => a < b)
+var MedianFinder = function () {
+    this.maxHeap = new MedianHeap((a, b) => a > b)
+    this.minHeap = new MedianHeap((a, b) => a < b)
 };
 
 /** 
  * @param {number} num
  * @return {void}
  */
-MedianFinder.prototype.addNum = function(num) {
+MedianFinder.prototype.addNum = function (num) {
     this.maxHeap.push(num)
-
     this.minHeap.push(this.maxHeap.pop())
 
     if(this.minHeap.size() > this.maxHeap.size()) {
@@ -88,12 +87,12 @@ MedianFinder.prototype.addNum = function(num) {
 /**
  * @return {number}
  */
-MedianFinder.prototype.findMedian = function() {
+MedianFinder.prototype.findMedian = function () {
     if(this.maxHeap.size() > this.minHeap.size()) {
         return this.maxHeap.peek()
     }
 
-    return (this.maxHeap.peek() + this.minHeap.peek()) / 2
+    return (this.minHeap.peek() + this.maxHeap.peek()) / 2
 };
 
 /** 
