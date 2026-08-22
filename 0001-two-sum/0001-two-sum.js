31c38f4e7f9052941570a345c46bd39b00f9873b
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-22 10:29:07 +0900

    Time: 0 ms (100%), Space: 54.4 MB (70.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = {}
+  const map = {}
 
-    for(let i = 0; i < nums.length; i++) {
-        const need = target - nums[i]
-
-        if(map[need] === undefined) {
-            map[nums[i]] = i
-        } else {
-            return [map[need], i]
-        }
+  for(let i = 0; i < nums.length; i++) {
+    const need= target - nums[i]
+    if(map[need] === undefined) {
+        map[nums[i]] = i
+    } else {
+        return [map[need], i]
     }
+  } 
 };
