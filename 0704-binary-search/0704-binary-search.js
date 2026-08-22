0f1da3cbeca1c809a0bc75b8901c29aa3265b68c
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-22 10:30:28 +0900

    Time: 0 ms (100%), Space: 58.3 MB (33.23%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
+    let left = 0
     let right = nums.length - 1
 
     while(left <= right) {
-        const mid = Math.floor((left + right) / 2)
+        const mid = Math.floor((left + right)/ 2)
 
         if(nums[mid] === target) {
             return mid
         } else if(nums[mid] < target) {
-            left++
+            left = mid + 1
         } else {
-            right--
+            right = mid - 1
         }
     }
     return -1
