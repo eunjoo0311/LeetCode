cadcfaaad546b3b7abe8153eb7a2e6239da583a4
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-22 09:33:59 +0900

    Time: 16 ms (35.1%), Space: 60.1 MB (92.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -5,25 +5,20 @@
  */
 var canFinish = function(numCourses, prerequisites) {
     const graph = Array.from({length : numCourses}, () => [])
-    // 선수 과목을 담을 배열 만들기
 
-    for(let [a, b] of prerequisites) {
+    for(let [a,b] of prerequisites) {
         graph[b].push(a)
     }
-    // 배열에 담기
 
     const visited = Array(numCourses).fill(0)
-    // 방문 여부 처리
 
     const dfs = (course) => {
         if(visited[course] === 1) return false
-        // 이미 방문 중이라면
         if(visited[course] === 2) return true
-        // 방문 끝이라면
 
         visited[course] = 1
 
-        for(let next of graph[course]) {
+        for(const next of graph[course]) {
             if(!dfs(next)) {
                 return false
             }
@@ -37,5 +32,6 @@ var canFinish = function(numCourses, prerequisites) {
             return false
         }
     }
+
     return true
 };
