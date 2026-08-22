a2dad666294ade7da3f5f28e5272a497d1655c9a
Eunjoo Lee <120389195+eunjoo0311@users.noreply.github.com>
2026-08-22 10:27:09 +0900

    Time: 3 ms (59.56%), Space: 59.1 MB (25.4%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -7,24 +7,22 @@ var insert = function(intervals, newInterval) {
     const answer = []
 
     let i = 0
-    let n = intervals.length
 
-    while(i < n && intervals[i][1] < newInterval[0]) {
+    while(i < intervals.length && intervals[i][1] < newInterval[0]) {
         answer.push(intervals[i])
         i++
     }
 
-    while(i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(intervals[i][0], newInterval[0])
-        newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+    while(i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1])
         i++
     }
     answer.push(newInterval)
 
-    while(i < n) {
+    while(i < intervals.length) {
         answer.push(intervals[i])
         i++
     }
-
     return answer
 };
